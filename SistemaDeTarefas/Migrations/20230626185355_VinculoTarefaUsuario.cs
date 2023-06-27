8039f50dfa813e756336f98423d22b11be55256c
Lucas Braz <lukkascomics@gmail.com>
2023-06-26 21:07:50 -0300

    Novas alterações

---
+++ SistemaDeTarefas/Migrations/20230626185355_VinculoTarefaUsuario.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaDeTarefas.Migrations
+{
+    public partial class VinculoTarefaUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Tarefas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tarefas_UserId",
+                table: "Tarefas",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tarefas_Usuarios_UserId",
+                table: "Tarefas",
+                column: "UserId",
+                principalTable: "Usuarios",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tarefas_Usuarios_UserId",
+                table: "Tarefas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tarefas_UserId",
+                table: "Tarefas");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Tarefas");
+        }
+    }
+}
